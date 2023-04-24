c92b7650def4856326fc8387b19e52c1f468d887
Emilio Encarnacion <emilio.encarnacion@tufts.edu>
2023-04-24 14:38:48 -0400

    Finished the pause menu
    Changes I made:
    -Moved the music on / off button to the pause menu
    -Put the controls / arrow keys in the pause menu
    -Made the pause menu able to toggle using the escape key

--- Crossings/Assets/Scripts/PauseMenu.cs
+++ Crossings/Assets/Scripts/PauseMenu.cs
@@ -6,17 +6,35 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
+    public bool isPaused;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
 
     public void Pause()
     {
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
+        isPaused = true;
     }
 
     public void Resume()
     {
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
+        isPaused = false;
     }
 
     public void Home(int sceneID)
